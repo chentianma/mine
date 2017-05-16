05b04e3ec16e49ac49a6b21092028ac56d847ea5
Ivory <819252022@qq.com>
2017-05-16 09:55:46 +0800

    host='0.0.0.0'

--- mine.py
+++ mine.py
@@ -24,5 +24,5 @@ manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
     # manager.run()
